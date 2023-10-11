b446f775469830ed2d1ed37b8968b8849f1e8a06
Marcos <marcos.mb.mb@outlook.com>
2023-10-11 13:49:29 -0300

    Exercicio 2

---
+++ exec_2.js
@@ -0,0 +1,5 @@
+const converterIdadeEmAnosParaDias = idade => idade * 365;
+
+console.log(converterIdadeEmAnosParaDias(25));
+console.log(converterIdadeEmAnosParaDias(70));
+console.log(converterIdadeEmAnosParaDias(1));

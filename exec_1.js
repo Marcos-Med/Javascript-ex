42671330261b4bc4dbd0afbaa9878352a76a25f2
Marcos <marcos.mb.mb@outlook.com>
2023-10-11 13:46:22 -0300

    Atualizando 01

---
+++ exec_1.js
@@ -0,0 +1,6 @@
+const cumprimentar = pessoa => `Olá, ${pessoa}!`;
+
+console.log(cumprimentar('Maria'));
+console.log(cumprimentar('João'));
+console.log(cumprimentar('Leonardo'));
+console.log(cumprimentar('Marcos'));
